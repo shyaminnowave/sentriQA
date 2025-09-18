798fa06c5b81c43f4a99a39b886313c2cf65d7e4
shyam-kumar_Innowave <shyam.kumar@innowave.tech>
2025-09-18 17:59:45 +0530

    upadated

--- sentriQA/settings.py
+++ sentriQA/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "assets/"
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/dist/assets'),
